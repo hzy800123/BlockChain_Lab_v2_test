ace778d31fc280990e410d19828a42b73cfd2af2
john <hzy800@126.com>
2020-12-20 17:28:10 +0800

    amend test_example01.sh again

--- scripts/test_example01.sh
+++ scripts/test_example01.sh
@@ -42,7 +42,7 @@ peer chaincode query -C $CHANNEL_NAME -n $CC_NAME -c '{"Function":"Hi", "Args":[
 set +x
 
 
-# echo '######## - (ORG1) SetValue chaincode - ########'
+echo '######## - (ORG1) SetValue chaincode - ########'
 setupPeerENV1
 set -x
 if [[ "$CORE_PEER_TLS_ENABLED" == "true" ]]; then
@@ -78,12 +78,12 @@ if [[ "$CORE_PEER_TLS_ENABLED" == "true" ]]; then
     peer chaincode invoke \
     -o ${ORDERER_ADDRESS} --ordererTLSHostnameOverride orderer.example.com --tls $CORE_PEER_TLS_ENABLED --cafile $ORDERER_CA \
     -C $CHANNEL_NAME -n ${CC_NAME}  \
-    -c '{"Function":"SetValue","Args":["NewKey1_ORG2","NewValue3_ORG2"]}'    
+    -c '{"Function":"SetValue","Args":["NewKey1_ORG2","NewValue1_ORG2"]}'    
 else
     peer chaincode invoke \
     -o ${ORDERER_ADDRESS} \
     -C $CHANNEL_NAME -n ${CC_NAME}  \
-    -c '{"Function":"SetValue","Args":["NewKey1_ORG2","NewValue3_ORG2"]}'    
+    -c '{"Function":"SetValue","Args":["NewKey1_ORG2","NewValue1_ORG2"]}'    
 fi
 set +x
 sleep 10
@@ -92,18 +92,15 @@ sleep 10
 echo '######## - (ORG1) GetValue chaincode - ########'
 setupPeerENV1
 set -x
-peer chaincode query -C $CHANNEL_NAME -n $CC_NAME -c '{"Function":"GetValue", "Args":["NewKeyORG1"]}'
 peer chaincode query -C $CHANNEL_NAME -n $CC_NAME -c '{"Function":"GetValue", "Args":["NewKey1_ORG1"]}'
 peer chaincode query -C $CHANNEL_NAME -n $CC_NAME -c '{"Function":"GetValue", "Args":["NewKey2_ORG1"]}'
 set +x
 
 
-echo '######## - (ORG1) GetValue chaincode - ########'
+echo '######## - (ORG2) GetValue chaincode - ########'
 setupPeerENV2
 set -x
 peer chaincode query -C $CHANNEL_NAME -n $CC_NAME -c '{"Function":"GetValue", "Args":["NewKey1_ORG2"]}'
-peer chaincode query -C $CHANNEL_NAME -n $CC_NAME -c '{"Function":"GetValue", "Args":["NewKey2_ORG2"]}'
-peer chaincode query -C $CHANNEL_NAME -n $CC_NAME -c '{"Function":"GetValue", "Args":["NewKey3_ORG2"]}'
 set +x
 
 echo '############# END ###############'
