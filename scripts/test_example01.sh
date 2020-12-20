c3267ffff65131ff467116dee9731464be0f7c2d
john <hzy800@126.com>
2020-12-20 17:06:33 +0800

    amend test_example01.sh

--- scripts/test_example01.sh
+++ scripts/test_example01.sh
@@ -40,4 +40,70 @@ setupPeerENV2
 set -x
 peer chaincode query -C $CHANNEL_NAME -n $CC_NAME -c '{"Function":"Hi", "Args":[]}'
 set +x
+
+
+# echo '######## - (ORG1) SetValue chaincode - ########'
+setupPeerENV1
+set -x
+if [[ "$CORE_PEER_TLS_ENABLED" == "true" ]]; then
+    peer chaincode invoke \
+    -o ${ORDERER_ADDRESS} --ordererTLSHostnameOverride orderer.example.com --tls $CORE_PEER_TLS_ENABLED --cafile $ORDERER_CA \
+    -C $CHANNEL_NAME -n ${CC_NAME}  \
+    -c '{"Function":"SetValue","Args":["NewKey1_ORG1","NewValue1_ORG1"]}'    
+else
+    peer chaincode invoke \
+    -o ${ORDERER_ADDRESS} \
+    -C $CHANNEL_NAME -n ${CC_NAME}  \
+    -c '{"Function":"SetValue","Args":["NewKey1_ORG1","NewValue1_ORG1"]}'    
+fi
+
+if [[ "$CORE_PEER_TLS_ENABLED" == "true" ]]; then
+    peer chaincode invoke \
+    -o ${ORDERER_ADDRESS} --ordererTLSHostnameOverride orderer.example.com --tls $CORE_PEER_TLS_ENABLED --cafile $ORDERER_CA \
+    -C $CHANNEL_NAME -n ${CC_NAME}  \
+    -c '{"Function":"SetValue","Args":["NewKey2_ORG1","NewValue2_ORG1"]}'    
+else
+    peer chaincode invoke \
+    -o ${ORDERER_ADDRESS} \
+    -C $CHANNEL_NAME -n ${CC_NAME}  \
+    -c '{"Function":"SetValue","Args":["NewKey2_ORG1","NewValue2_ORG1"]}'    
+fi
+set +x
+
+
+echo '######## - (ORG2) query chaincode - ########'
+setupPeerENV2
+set -x
+if [[ "$CORE_PEER_TLS_ENABLED" == "true" ]]; then
+    peer chaincode invoke \
+    -o ${ORDERER_ADDRESS} --ordererTLSHostnameOverride orderer.example.com --tls $CORE_PEER_TLS_ENABLED --cafile $ORDERER_CA \
+    -C $CHANNEL_NAME -n ${CC_NAME}  \
+    -c '{"Function":"SetValue","Args":["NewKey1_ORG2","NewValue3_ORG2"]}'    
+else
+    peer chaincode invoke \
+    -o ${ORDERER_ADDRESS} \
+    -C $CHANNEL_NAME -n ${CC_NAME}  \
+    -c '{"Function":"SetValue","Args":["NewKey1_ORG2","NewValue3_ORG2"]}'    
+fi
+set +x
+sleep 10
+
+
+echo '######## - (ORG1) GetValue chaincode - ########'
+setupPeerENV1
+set -x
+peer chaincode query -C $CHANNEL_NAME -n $CC_NAME -c '{"Function":"GetValue", "Args":["NewKeyORG1"]}'
+peer chaincode query -C $CHANNEL_NAME -n $CC_NAME -c '{"Function":"GetValue", "Args":["NewKey1_ORG1"]}'
+peer chaincode query -C $CHANNEL_NAME -n $CC_NAME -c '{"Function":"GetValue", "Args":["NewKey2_ORG1"]}'
+set +x
+
+
+echo '######## - (ORG1) GetValue chaincode - ########'
+setupPeerENV2
+set -x
+peer chaincode query -C $CHANNEL_NAME -n $CC_NAME -c '{"Function":"GetValue", "Args":["NewKey1_ORG2"]}'
+peer chaincode query -C $CHANNEL_NAME -n $CC_NAME -c '{"Function":"GetValue", "Args":["NewKey2_ORG2"]}'
+peer chaincode query -C $CHANNEL_NAME -n $CC_NAME -c '{"Function":"GetValue", "Args":["NewKey3_ORG2"]}'
+set +x
+
 echo '############# END ###############'
